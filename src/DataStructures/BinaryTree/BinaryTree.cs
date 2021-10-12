1d0018cc9fe3236e230d3d23bc3b699a63748473
sirDudesALot <johnclivejonesuk@hotmail.co.uk>
2021-10-12 13:17:32 +0100

    Added binary tree

---
+++ src/DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinaryTree
+{
+    public class BinaryTree
+    {
+        private Node _root;
+
+        public BinaryTree Add(int data)
+        {
+            var node = new Node(data);
+            if(_root is null)
+            {
+                _root = node;
+            }
+            else
+            {
+                _root.Add(node);
+            }
+            return this;
+        }
+
+        public void ForEach(Action<Node> action)
+        {
+            if(_root is not null)
+            {
+                _root.ForEach(action);
+            }
+        }
+    }
+}
---
+++ src/DataStructures/BinaryTree/Node.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinaryTree
+{
+    public class Node
+    {
+        private int _data;
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int data) => _data = data;
+
+        public void Add(Node node)
+        {
+            if(node._data < this._data)
+            {
+                if(this.Left is null)
+                {
+                    this.Left = node;
+                }
+                else
+                {
+                    this.Left.Add(node);
+                }
+            }
+            else
+            {
+                if(this.Right is null)
+                {
+                    this.Right = node;
+                }
+                else
+                {
+                    this.Right.Add(node);
+                }
+            }
+        }
+
+        public void ForEach(Action<Node> action)
+        {
+            if(this.Left is not null)
+            {
+                this.Left.ForEach(action);
+            }
+            action(this);
+            if(this.Right is not null)
+            {
+                this.Right.ForEach(action);
+            }
+        }
+
+        public override string ToString() => _data.ToString();
+    }
+}
