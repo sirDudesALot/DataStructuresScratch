379825b649f5fe90bfee82c2d469b1b9b1fe8da0
sirDudesALot <johnclivejonesuk@hotmail.co.uk>
2021-10-12 13:00:10 +0100

    Added linked list

---
+++ src/DataStructures/LinkedList/LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList
+{
+    public class LinkedList
+    {
+        private Node _head;
+
+        public LinkedList Add(int data)
+        {
+            var node = new Node(data);
+            if(_head is null)
+            {
+                _head = node;
+            }
+            else
+            {
+                _head.Add(node);
+            }
+            return this;
+        }
+
+        public LinkedList Reverse()
+        {
+            var current = _head;
+            Node previous = null;
+            while(current is not null)
+            {
+                var next = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = next;
+            }
+            _head = previous;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var current = _head;
+            while(current is not null)
+            {
+                sb.Append($"{current}{(current.Next is not null ? " -> " : string.Empty)}");
+                current = current.Next;
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/DataStructures/LinkedList/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList
+{
+    internal class Node
+    {
+        private int _data;
+        internal Node Next { get; set; }
+
+        internal Node(int data) => _data = data;
+
+        internal void Add(Node node)
+        {
+            if(Next is null)
+            {
+                Next = node;
+            }
+            else
+            {
+                Next.Add(node);
+            }
+        }
+
+        public override string ToString() => _data.ToString();
+    }
+}
